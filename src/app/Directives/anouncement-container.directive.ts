38a93aa3c57c4bb8568b9f90265e7e5884886cc5
RonStrauss <ashbelthor@gmail.com>
2024-11-15 17:45:52 +0200

    remove log

--- src/app/Directives/anouncement-container.directive.ts
+++ src/app/Directives/anouncement-container.directive.ts
@@ -9,7 +9,6 @@ export class AnouncementContainerDirective implements AfterViewInit {
 	constructor(private element: ElementRef, private renderer: Renderer2, private _anouncer: AnouncerService) {}
 
 	ngAfterViewInit(): void {
-		console.log('ngoninit');
 		this.renderer.setAttribute(this.element.nativeElement, 'aria-live', 'polite');
 		this.renderer.setAttribute(this.element.nativeElement, 'aria-atomic', 'true');
 		this.renderer.setStyle(this.element.nativeElement, 'width', '1px');
