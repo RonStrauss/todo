98337a4a1d5a34c08b892a73a4bc2e10dd99a31e
RonStrauss <ashbelthor@gmail.com>
2024-11-15 12:25:27 +0200

    add unit tests

--- src/app/components/todo-item/todo-item.component.spec.ts
+++ src/app/components/todo-item/todo-item.component.spec.ts
@@ -1,22 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TodoItemComponent } from './todo-item.component';
+import { Todo } from 'src/app/interfaces/todo';
+import { TodosService } from 'src/app/services/todos.service';
+
+export const dummyTodo: Todo = {
+	id: '1',
+	title: 'Test',
+	completed: false,
+	timestamp: Date.now(),
+};
 
 describe('TodoItemComponent', () => {
 	let component: TodoItemComponent;
 	let fixture: ComponentFixture<TodoItemComponent>;
+	let todosService: TodosService;
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [TodoItemComponent],
+			imports: [TodoItemComponent],
+			providers: [
+				{
+					provide: TodosService,
+					useValue: {
+						toggleTodoById: jasmine.createSpy('toggleTodoById'),
+						removeTodoById: jasmine.createSpy('removeTodoById'),
+					},
+				},
+			],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(TodoItemComponent);
 		component = fixture.componentInstance;
+		todosService = TestBed.inject(TodosService);
+		component.todo = dummyTodo;
 		fixture.detectChanges();
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should throw error if todo input is not provided', () => {
+		component.todo = undefined as any;
+		expect(() => component.ngOnInit()).toThrowError('todo input is required');
+	});
+
+	it('should call toggleTodoById when toggle is called', () => {
+		component.toggle();
+		expect(todosService.toggleTodoById).toHaveBeenCalledWith(dummyTodo.id);
+	});
+
+	it('should call removeTodoById when remove is called', () => {
+		component.remove();
+		expect(todosService.removeTodoById).toHaveBeenCalledWith(dummyTodo.id);
 	});
 });
--- src/app/components/todo-list/todo-list.component.spec.ts
+++ src/app/components/todo-list/todo-list.component.spec.ts
@@ -1,22 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TodoListComponent } from './todo-list.component';
+import { TodosService } from 'src/app/services/todos.service';
+import { of } from 'rxjs';
 
 describe('TodoListComponent', () => {
 	let component: TodoListComponent;
 	let fixture: ComponentFixture<TodoListComponent>;
+	let todosService: TodosService;
 
 	beforeEach(async () => {
+		const todosServiceMock = {
+			todos$: of([
+				{ id: '1', title: 'Test Todo 1', completed: false, timestamp: Date.now() },
+				{ id: '2', title: 'Test Todo 2', completed: true, timestamp: Date.now() },
+			]),
+		};
+
 		await TestBed.configureTestingModule({
-			declarations: [TodoListComponent],
+			imports: [TodoListComponent],
+			providers: [{ provide: TodosService, useValue: todosServiceMock }],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(TodoListComponent);
 		component = fixture.componentInstance;
+		todosService = TestBed.inject(TodosService);
 		fixture.detectChanges();
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should have role attribute set to list', () => {
+		const hostElement: HTMLElement = fixture.nativeElement;
+		expect(hostElement.getAttribute('role')).toBe('list');
+	});
+
+	it('should display todos', done => {
+		component.todos$.subscribe(todos => {
+			expect(todos.length).toBe(2);
+			expect(todos[0].title).toBe('Test Todo 1');
+			expect(todos[1].title).toBe('Test Todo 2');
+			done();
+		});
 	});
 });
